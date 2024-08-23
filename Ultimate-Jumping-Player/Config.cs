ddd39b932bf5c85c8763a996eec69efbf44e7103
15Slamyxx <140884344+15Slamyxx@users.noreply.github.com>
2024-08-23 10:27:49 +0200

    Initial commit

---
+++ Ultimate-Jumping-Player/Config.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace Ultimate_Jumping_Player;
+public class Config : IConfig
+{
+    public bool IsEnabled { get; set; } = true;
+	
+    public bool Debug { get; set; }
+    
+    [Description("Remove stamina for player jump please put f at the end")]
+    public float RemoveStamina { get; set; } = 0.15f;
+    
+    [Description("Hint for player jump")]
+    public string MessageHint { get; set; } = "<b><size=22>please don't jump</size></b>";
+
+    [Description("Duration Hint")] 
+    public float DurationHint { get; set; } = 5f;
+}
---
+++ Ultimate-Jumping-Player/EventHandlers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using Exiled.API.Enums;
+
+namespace Ultimate_Jumping_Player;
+public class EventHandlers : Plugin<Config>
+{
+    
+    public override string Author => "RomzyyTV";
+    public override string Name => "Ultimate-Jumping-Player";
+    public override Version Version => new Version(1, 0, 0);
+    public override Version RequiredExiledVersion { get; } = new(8, 11, 0);
+    
+    
+    public override void OnEnabled()
+    {
+        Exiled.Events.Handlers.Player.Jumping += OnPlayerJumping;
+        base.OnEnabled();
+    }
+    
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Player.Jumping -= OnPlayerJumping;
+        base.OnDisabled();
+    }
+    
+    private void OnPlayerJumping(JumpingEventArgs ev)
+    {
+        if (!ev.Player.IsScp)
+            ev.Player.Stamina -= Config.RemoveStamina;
+            ev.Player.ShowHint(Config.MessageHint, Config.DurationHint);
+    }
+}
