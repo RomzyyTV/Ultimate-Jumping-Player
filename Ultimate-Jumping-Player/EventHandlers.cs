f6dcdfe1acd70e6a1114f665008e17d056abd583
15Slamyxx <140884344+15Slamyxx@users.noreply.github.com>
2024-10-11 22:03:57 +0200

    Change EventHandlers.cs When a player has scp 207 he does not lose stamina

--- Ultimate-Jumping-Player/EventHandlers.cs
+++ Ultimate-Jumping-Player/EventHandlers.cs
@@ -1,34 +1,17 @@
-﻿using System;
-using Exiled.API.Features;
+﻿using Exiled.API.Enums;
 using Exiled.Events.EventArgs.Player;
-using Exiled.API.Enums;
+using InventorySystem.Items.Usables;
 
 namespace Ultimate_Jumping_Player;
-public class EventHandlers : Plugin<Config>
+public class EventHandlers
 {
-    
-    public override string Author => "RomzyyTV";
-    public override string Name => "Ultimate-Jumping-Player";
-    public override Version Version => new Version(1, 0, 0);
-    public override Version RequiredExiledVersion { get; } = new(8, 11, 0);
-    
-    
-    public override void OnEnabled()
+    public void OnPlayerJumping(JumpingEventArgs ev)
     {
-        Exiled.Events.Handlers.Player.Jumping += OnPlayerJumping;
-        base.OnEnabled();
-    }
-    
-    public override void OnDisabled()
-    {
-        Exiled.Events.Handlers.Player.Jumping -= OnPlayerJumping;
-        base.OnDisabled();
-    }
-    
-    private void OnPlayerJumping(JumpingEventArgs ev)
-    {
+        if (ev.Player.GetEffect(EffectType.Scp207).IsEnabled) return;
         if (!ev.Player.IsScp)
-            ev.Player.Stamina -= Config.RemoveStamina;
-            ev.Player.ShowHint(Config.MessageHint, Config.DurationHint);
+        {
+            ev.Player.Stamina -= Plugin.Singleton.Config.RemoveStamina;
+            ev.Player.ShowHint(Plugin.Singleton.Config.MessageHint, Plugin.Singleton.Config.DurationHint);
+        }
     }
 }
---
+++ Ultimate-Jumping-Player/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace Ultimate_Jumping_Player;
+public class Plugin : Plugin<Config>
+{
+    public override string Author => "RomzyyTV";
+    public override string Name => "Ultimate-Jumping-Player";
+    public override Version Version => new Version(1, 0, 1);
+    public override Version RequiredExiledVersion { get; } = new(8, 12, 2);
+    public static Plugin Singleton;
+    public static EventHandlers Handlers;
+    
+    public override void OnEnabled()
+    {
+        Singleton = this;
+        Handlers = new EventHandlers();
+        
+        Exiled.Events.Handlers.Player.Jumping += Handlers.OnPlayerJumping;
+        base.OnEnabled();
+    }
+    
+    public override void OnDisabled()
+    {
+        Singleton = null;
+        Handlers = null;
+        
+        Exiled.Events.Handlers.Player.Jumping -= Handlers.OnPlayerJumping;
+        base.OnDisabled();
+    }
+}
